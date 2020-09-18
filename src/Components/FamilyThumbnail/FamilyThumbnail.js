4de32db4d77b9b4dd753c1867915c69337e7517e
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-09-18 01:39:35 -0500

    add additional child user info from firebase auth data

--- src/Components/FamilyThumbnail/FamilyThumbnail.js
+++ src/Components/FamilyThumbnail/FamilyThumbnail.js
@@ -6,7 +6,7 @@ import utils from '../../helpers/utils';
 const FamilyThumbnails = (props) => (
   <div className="d-flex flex-column justify-content-between text-center">
     <img className="addBlankUser" src={props.member.photoURL ? props.member.photoURL : blankUser} alt="Face" id={props.member.id} />
-    <h5>{props.member.displayName ? utils.firstName(props.members.displayName) : `${props.member.email.substring(0, 6)}...`}</h5>
+    <h5>{props.member.name ? utils.firstName(props.member.name) : `${props.member.email.substring(0, 6)}...`}</h5>
   </div>);
 
 export default FamilyThumbnails;
